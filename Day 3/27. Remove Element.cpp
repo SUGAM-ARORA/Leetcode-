cb604c814e168ecc8bc0bf44ca73c1bc478bd3fe
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-01-31 02:01:32 +0530

    Create 27. Remove Element.cpp

---
+++ Day 3/27. Remove Element.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+	auto it = remove_if(nums.begin(), nums.end(), [&](int i){return i==val;});
+	return distance(nums.begin(), it);
+    }
+};
