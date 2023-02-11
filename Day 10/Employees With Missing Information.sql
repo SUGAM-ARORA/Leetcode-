6632cc763f0e329661e7c5c32bb6b75362e99ed0
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-02-12 01:12:18 +0530

    Create Employees With Missing Information.sql

---
+++ Day 10/Employees With Missing Information.sql
@@ -0,0 +1,4 @@
+SELECT employee_id from Employees e WHERE employee_id not in (Select employee_id from Salaries)
+UNION 
+SELECT employee_id from Salaries s WHERE employee_id not in (Select employee_id from Employees)
+ORDER BY employee_id;
