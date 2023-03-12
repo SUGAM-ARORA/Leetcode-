f046c034ea7ec39a4333b8e53a624c70ab7a353d
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-03-12 10:35:11 +0530

    Create Merge k Sorted Lists.cpp

---
+++ Day 19/Merge k Sorted Lists.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return NULL;
+
+        ListNode* dummyHead = new ListNode(-1);
+        ListNode* dummyTail = dummyHead;
+
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for(auto head : lists)  if(head != NULL) pq.push({head->val, head});
+
+        while(!pq.empty()){
+            ListNode* minNode = pq.top().second;
+            pq.pop();
+            if(minNode->next != NULL) pq.push({minNode->next->val, minNode->next});
+
+            dummyTail->next = minNode;
+            dummyTail = dummyTail->next;
+        }
+        return dummyHead->next;
+    }
+};
