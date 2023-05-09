c554542440b0e2e74523b3a901d3a173a4ec3eae
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-05-09 18:02:29 +0530

    Time: 59 ms (51.50%), Space: 42.1 MB (50.33%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter((x,i)=>fn(x,i));
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
