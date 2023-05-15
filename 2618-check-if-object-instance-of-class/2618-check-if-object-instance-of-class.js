0ef2b62a10fb98e3c0caed851cadf8bfc6e3ab71
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-05-16 00:43:34 +0530

    Time: 106 ms (65.30%), Space: 51.4 MB (92.14%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) 
+{
+    if(obj === null || obj === undefined || typeof classFunction !== "function")
+        return false;
+    return Object(obj) instanceof classFunction;
+
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
