31925cc0f7f954c0fa534ee69db3287419f16632
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-05-28 22:33:24 +0530

    Time: 132 ms (94.87%), Space: 65.7 MB (34.83%) - LeetHub

---
+++ 2631-group-by/2631-group-by.ts
@@ -0,0 +1,25 @@
+declare global {
+  interface Array<T> {
+    groupBy(fn: (item: T) => string): Record<string, T[]>;
+  }
+}
+
+Array.prototype.groupBy = function(fn) {
+  return this.reduce((grouped, item) => {
+    const key = fn(item);
+    if (!(key in grouped)) {
+      grouped[key] = [];
+    }
+    grouped[key].push(item);
+    return grouped;
+  }, {});
+};
+
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
