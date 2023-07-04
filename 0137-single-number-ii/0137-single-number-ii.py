8c9b1ae669feabd8480d4416330fffb9b99beffa
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-07-04 14:38:47 +0530

    Time: 72 ms (77.49%), Space: 18.5 MB (20.63%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums):
+        count = defaultdict(int)
+        
+        for x in nums:
+            count[x] += 1
+
+        for x, freq in count.items():
+            if freq == 1:
+                return x
+        
+        return -1
+        
