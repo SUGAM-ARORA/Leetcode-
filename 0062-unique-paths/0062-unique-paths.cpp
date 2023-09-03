5c18a214db7701d2634ef6a4e73b167c68e4d0a8
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-09-03 18:06:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,41 +1,14 @@
-class Solution {
-private:
-    // Private helper function for calculating unique paths recursively with memoization
-    int solve(int i, int j, int m, int n, vector<vector<int>> &dp) 
-    {
-        // Base case: If we are out of bounds, return 0 (no valid path)
-        if (i >= m || j >= n) 
-        {
-            return 0;
-        }
-        
-        // Base case: If we've reached the bottom-right corner, return 1 (one valid path)
-        if (i == m - 1 && j == n - 1) 
-        {
-            return 1;
-        }
-        
-        // If the result for the current position (i, j) has already been calculated, return it
-        if (dp[i][j] != -1) 
-        {
-            return dp[i][j];
-        }
-        
-        // Calculate the number of unique paths by moving right and down recursively
-        dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
-        
-        // Return the calculated result for this position
-        return dp[i][j];
-    }
-
+#include <cmath>
+class Solution 
+{
 public:
-    // Public function to calculate unique paths in an m x n grid
     int uniquePaths(int m, int n) 
     {
-        // Create a memoization table (dp) with dimensions (m+1) x (n+1)
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        
-        // Call the solve function with initial position (0, 0) and the memoization table
-        return solve(0, 0, m, n, dp);
+        long long ans = 1;
+        for (int i = 1; i <= m - 1; ++i) 
+        {
+            ans = ans * (n - 1 + i) / i;
+        }
+        return (int)ans;
     }
 };
