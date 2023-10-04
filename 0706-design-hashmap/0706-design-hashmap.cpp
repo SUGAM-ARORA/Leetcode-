88dc3b36c677f999ce22faefc3ee3f09894ceb54
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-10-04 11:25:38 +0530

    Time: 180 ms (12.02%), Space: 50.7 MB (96.52%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,45 @@
+class MyHashMap {
+private:
+    std::vector<std::pair<int, int>> map;
+
+public:
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        for (auto& pair : map) {
+            if (pair.first == key) {
+                pair.second = value; 
+                return;
+            }
+        }
+        map.push_back(std::make_pair(key, value)); 
+    }
+    
+    int get(int key) {
+        for (const auto& pair : map) {
+            if (pair.first == key) {
+                return pair.second; 
+            }
+        }
+        return -1; 
+    }
+    
+    void remove(int key) {
+        for (auto it = map.begin(); it != map.end(); ++it) {
+            if (it->first == key) {
+                map.erase(it); 
+                return;
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
