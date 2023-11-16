06469d0ee41fa5ade4850a71c3ad45fb339095ac
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-11-16 11:36:01 +0530

    Time: 40 ms (70.29%), Space: 16.2 MB (86.18%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        result = ""
+
+        for i in range(len(nums)):
+            result += '1' if nums[i][i] == '0' else '0'
+
+        return result
+        
+        
